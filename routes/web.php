f4fe94cc8467abcd4cb20fb0dc1ab8e83960bf67
Prateek Kher <prateek.kher@gmail.com>
2021-04-20 14:35:07 +0530

    Added Routes for Product, Pos-Item, Templates

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Models\Ledger;
+use App\Models\PosItem;
+use App\Models\PosTemplate;
+use App\Models\Product;
 use App\Models\User;
-use Carbon\Carbon;
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
@@ -31,12 +32,41 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 $router->group(['prefix'=>'users'], function() use ($router) {
     $router->post('login', ['uses' => 'UserController@login']);
     $router->put('create', ['uses' => 'UserController@register']);
-    $router->get('/', function() {
-        return response()->json(User::all());
-    });
 });
 
 $router->group(['prefix'=>'ledgers'], function() use ($router) {
-    $router->get('/', ['uses' => 'LedgerController@select']);
+    $router->get('', ['uses' => 'LedgerController@select']);
     $router->put('create', ['uses' => 'LedgerController@create']);
+    $router->post('update', ['uses' => 'LedgerController@update']);
+});
+
+$router->group(['prefix'=>'products'], function() use ($router) {
+    $router->get('', function() {
+        return response()->json(Product::all());
+    });
+    $router->put('create', ['uses' => 'ProductController@create']);
+    $router->post('update', ['uses' => 'ProductController@update']);
+    $router->delete('delete/{id}', ['uses' => 'ProductConroller@delete']);
+});
+
+$router->group(['prefix'=>'pos-items'], function() use ($router) {
+    $router->get('', ['uses' => 'PosItemController@select']);
+    $router->put('create', ['uses' => 'PosItemController@create']);
+    $router->post('update', ['uses' => 'PosItemController@update']);
+    $router->delete('delete/{id}', function($id) {
+        $posItem = PosItem::findOrFail($id);
+        PosTemplate::where('positem_id', $posItem->id)->delete();
+        $posItem->delete();
+
+        return response()->json(['message'=>'PosItem Deleted Successfully']);
+    });
+});
+
+$router->group(['prefix'=>'template'], function () use ($router) {
+    $router->put('create', ['uses' => 'PosTemplateController@create']);
+    $router->post('update', ['uses' => 'PosTemplateController@update']);
+    $router->delete('delete/{id}', function($id) {
+        PosTemplate::findOrFail($id)->delete();
+        return response()->json(['message' => 'Template Deleted Success']);
+    });
 });
