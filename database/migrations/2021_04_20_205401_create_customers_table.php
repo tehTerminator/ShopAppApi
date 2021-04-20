3d32220c9199df3d647ee7b10b6bc676a8183dbb
Prateek Kher <prateek.kher@gmail.com>
2021-04-20 20:59:36 +0530

    Added Columns to Customer Migration

--- database/migrations/2021_04_20_205401_create_customers_table.php
+++ database/migrations/2021_04_20_205401_create_customers_table.php
@@ -15,6 +15,8 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('address');
             $table->timestamps();
         });
     }
