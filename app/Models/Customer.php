b5538203bb6994e51379a51881ceb9299bb3720f
Prateek Kher <prateek.kher@gmail.com>
2021-04-20 21:09:46 +0530

    Added Relationship with Invoice

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -14,4 +14,8 @@ class Customer extends Model {
     protected $fillable = [
         'title', 'address'
     ];
+
+    public function invoices() {
+        return $this->hasMany(Invoice::class);
+    }
 }
