c0fc7e83b928d8addd3368b24e3a6661e4f055d1
Prateek Kher <prateek.kher@gmail.com>
2021-05-04 22:01:32 +0530

    File Created

---
+++ app/Models/Transaction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'invoice_id',
+        'item_id',
+        'item_type',
+        'description',
+        'quantity',
+        'rate',
+        'discount',
+        'user_id'
+    ];
+
+    public function invoice() {
+        return $this->hasOne(Invoice::class, 'id', 'invoice_id');
+    }
+
+    public function user() {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+}
