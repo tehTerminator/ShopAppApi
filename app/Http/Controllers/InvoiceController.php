bf299bd38ed7cd1a5396883af97430f875729d82
Prateek Kher <prateek.kher@gmail.com>
2021-06-27 12:59:39 +0530

    Moved Code From Controller to Service

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Invoice;
-use App\Models\Transaction;
-use App\Models\Ledger;
-use App\Models\Voucher;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Services\InvoiceService;
 
 class InvoiceController extends Controller
 {
+    private $service;
     /**
      * Create a new controller instance.
      *
@@ -19,19 +17,22 @@ class InvoiceController extends Controller
     public function __construct()
     {
         //
+        $this->service = new InvoiceService();
     }
 
     public function select(Request $request) {
         if ($request->has('id')) {
-            $invoice = Invoice::where('id', $request->query('id'))->with(['customer', 'transactions'])->first();
-            return response()->json($invoice);
+            $id = $request->id;
+            return response()->json($this->service->getInvoiceById($id));
         }
 
         if ($request->has('createdAt')) {
-            $invoices = Invoice::whereDate('created_at', $request->query('createdAt'))
-            ->where('user_id', $request->query('userId'))
-            ->with(['customer'])->get();
-            return response()->json($invoices);
+            $user_id = $request->userId;
+            $created_at = $request->createdAt;
+            
+            return response()->json(
+                $this->service->getInvoiceByDate($user_id, $created_at)
+            );
         }
 
         return response()->json(['status'=>'Please Either Provide Date or Id']);
@@ -43,23 +44,7 @@ class InvoiceController extends Controller
             'paymentMethod' => 'required',
             'amount' => 'required|numeric|min:1',
         ]);
-        
-        $user_id = Auth::user()->id;
-        
-        $invoice = Invoice::create([
-            'customer_id' => $request->input('customer_id'),
-            'user_id' => $user_id,
-            'paid' => $request->input('paid'),
-            'paymentMethod' => $request->input('paymentMethod'),
-            'amount' => $request->input('amount'),
-        ]);
 
-        $this->createTransactions($request, $invoice->id);
-        $this->createPaymentVoucher($request->get('transactions'), $invoice->id);
-        $this->createReceiptVoucher($invoice->paymentMethod, $invoice->id, $invoice->amount);
-    }
-
-    private function createTransactions(Request $request, $invoice_id) {
         $this->validate($request, [
             'transactions.*.item_id' => 'required|integer|min:1',
             'transactions.*.item_type' => 'required|in:LEDGER,PRODUCT',
@@ -68,87 +53,9 @@ class InvoiceController extends Controller
             'transaction.*.rate' => 'required|numeric|min:1',
             'transaction.*.discount' => 'required|numeric|min:0|max:100'
         ]);
-
+        
         $user_id = Auth::user()->id;
-
-        $transactions = $request->get('transactions');
-        for($i=0; $i < count($transactions); $i++) {
-            $transactions[$i]['invoice_id'] = $invoice_id;
-            $transactions[$i]['user_id'] = $user_id;
-            Transaction::create($transactions[$i]);
-        }
-    }
-
-    private function createPaymentVoucher($transactions, $invoice_id) {
-        $customer = Ledger::where('title', 'Customer')->where('kind', 'RECEIVABLES')->first();
-        if (!$customer) {
-            $customer = Ledger::create([
-                'title' => 'Customer',
-                'kind' => 'RECEIVABLES'
-            ]);
-        }
-
-        $sales = Ledger::where('title', 'Sales')->first();
-        if (!$sales) {
-            $sales = Ledger::create([
-                'title' => 'Sales',
-                'kind' => 'INCOME'
-            ]);
-        }
-
-        $saleAmount = 0;
-        $user_id = Auth::user()->id;
-
-        for ($i = 0; $i < count($transactions); $i++) {
-            if ($transactions[$i]['item_type'] === 'LEDGER') {
-                Voucher::create([
-                    'cr' => $transactions[$i]['item_id'],
-                    'dr' => $customer->id,
-                    'narration' => 'Payment Invoice #' . $invoice_id,
-                    'amount' => $this->getAmount($transactions[$i]),
-                    'user_id' => $user_id
-                ]);
-            } else {
-                $saleAmount += $this->getAmount($transactions[$i]);
-            }
-        }
-
-        if ($saleAmount > 0) {
-            Voucher::create([
-                'cr' => $sales->id,
-                'dr' => $customer->id,
-                'narration' => 'Sale Invoice #' . $invoice_id,
-                'amount' => $saleAmount,
-                'user_id' => $user_id
-            ]);
-        }
-    }
-
-    private function createReceiptVoucher($paymentMethod, $invoice_id, $amount) {
-        $user_id = Auth::user()->id;
-        $customer = Ledger::where('title', 'Customer')->where('kind', 'RECEIVABLES')->first();
-        if (!$customer) {
-            $customer = Ledger::create([
-                'title' => 'Customer',
-                'kind' => 'RECEIVABLES'
-            ]);
-        }
-
-        if ($paymentMethod <> 'UDHAAR') {
-            $receiver_id = explode('#', $paymentMethod)[1];
-            Voucher::create([
-                'cr' => $customer->id,
-                'dr' => $receiver_id,
-                'narration' => 'Payment Receipt for Invoice #' . $invoice_id,
-                'amount' => $amount,
-                'user_id' => $user_id
-            ]);
-        }
-
-        return response()->json(['status' => 'Success']);
-    }
-
-    private function getAmount($transaction) {
-        return ($transaction['quantity'] * $transaction['rate']) * (1 - $transaction['discount']/100);
+        
+        $this->service->createNewInvoice($request, $user_id);
     }
 }
---
+++ app/Services/InvoiceService.php
@@ -0,0 +1,153 @@
+<?php 
+
+namespace App\Services;
+
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+use App\Models\Ledger;
+use App\Models\Transaction;
+use App\Models\Voucher;
+
+class InvoiceService {
+
+    public function getInvoiceById(int $id) {
+        $invoice = Invoice::where('id', $id)
+        ->with(['customer', 'transactions'])
+        ->first();
+        return $invoice;
+    }
+
+    public function getInvoiceByDate(int $user, $created_at) {
+        $invoices = Invoice::whereDate('created_at', $created_at)
+            ->where('user_id', $user)
+            ->with(['customer'])->get();
+
+        return $invoices;
+    }
+
+    public function createNewInvoice(Request $request, int $user_id) {
+        $invoice = Invoice::create([
+            'customer_id' => $request->input('customer_id'),
+            'user_id' => $user_id,
+            'paid' => $request->input('paid'),
+            'paymentMethod' => $request->input('paymentMethod'),
+            'amount' => $request->input('amount'),
+        ]);
+        $this->createTransactions(
+            $request->get('transactions'), 
+            $invoice->id, 
+            $user_id
+        );
+        $this->createPaymentVoucher(
+            $request->get('transactions'), 
+            $invoice->id, 
+            $user_id
+        );
+        $this->createReceiptVoucher(
+            $invoice->paymentMethod, 
+            $invoice->id,
+            $invoice->amount, 
+            $user_id
+        );
+    }
+
+    private function createTransactions(
+        array $transactions, 
+        int $invoice_id, 
+        int $user_id
+    ) 
+    {
+        for($i=0; $i < count($transactions); $i++) {
+            $transactions[$i]['invoice_id'] = $invoice_id;
+            $transactions[$i]['user_id'] = $user_id;
+            Transaction::create($transactions[$i]);
+        }
+    }
+
+    private function createPaymentVoucher(
+        array $transactions, 
+        $invoice_id, 
+        $user_id
+    ) 
+    {
+        $customer = Ledger::where('title', 'Customer')
+        ->where('kind', 'RECEIVABLES')->first();
+        if (!$customer) {
+            $customer = Ledger::create([
+                'title' => 'Customer',
+                'kind' => 'RECEIVABLES'
+            ]);
+        }
+
+        $sales = Ledger::where('title', 'Sales')->first();
+        if (!$sales) {
+            $sales = Ledger::create([
+                'title' => 'Sales',
+                'kind' => 'INCOME'
+            ]);
+        }
+
+        $saleAmount = 0;
+
+        for ($i = 0; $i < count($transactions); $i++) {
+            if ($transactions[$i]['item_type'] === 'LEDGER') {
+                Voucher::create([
+                    'cr' => $transactions[$i]['item_id'],
+                    'dr' => $customer->id,
+                    'narration' => 'Payment Invoice #' . $invoice_id,
+                    'amount' => $this->getAmount($transactions[$i]),
+                    'user_id' => $user_id
+                ]);
+            } else {
+                $saleAmount += $this->getAmount($transactions[$i]);
+            }
+        }
+
+        if ($saleAmount > 0) {
+            Voucher::create([
+                'cr' => $sales->id,
+                'dr' => $customer->id,
+                'narration' => 'Sale Invoice #' . $invoice_id,
+                'amount' => $saleAmount,
+                'user_id' => $user_id
+            ]);
+        }
+    }
+
+    private function createReceiptVoucher(
+        string $paymentMethod, 
+        int $invoice_id, 
+        float $amount, 
+        int $user_id
+    ) 
+    {
+        $customer = Ledger::where('title', 'Customer')->where('kind', 'RECEIVABLES')->first();
+        if (!$customer) {
+            $customer = Ledger::create([
+                'title' => 'Customer',
+                'kind' => 'RECEIVABLES'
+            ]);
+        }
+
+        if ($paymentMethod <> 'UDHAAR') {
+            $receiver_id = explode('#', $paymentMethod)[1];
+            Voucher::create([
+                'cr' => $customer->id,
+                'dr' => $receiver_id,
+                'narration' => 'Receipt Invoice #' . $invoice_id,
+                'amount' => $amount,
+                'user_id' => $user_id
+            ]);
+        }
+
+        return response()->json(['status' => 'Success']);
+    }
+
+    private function getAmount($transaction) {
+        return 
+            ($transaction['quantity'] * $transaction['rate']) 
+            * (1 - $transaction['discount']/100);
+    }
+
+    public function __construct(){}
+}
