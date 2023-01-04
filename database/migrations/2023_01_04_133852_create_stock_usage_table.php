4459d4d2de4787e396ca9a69ab0e390f0462265b
Prateek Kher <prateek.kher@gmail.com>
2023-01-04 20:14:37 +0530

    Created Stock Usage Table

---
+++ database/migrations/2023_01_04_133852_create_stock_usage_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockUsageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock_usage', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('stock_item_id');
+            $table->foreign('stock_item_id')->references('id')->on('stock_items');
+            $table->double('quantity');
+            $table->string('narration')->nullable()->default(NULL);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock_usage');
+    }
+}
