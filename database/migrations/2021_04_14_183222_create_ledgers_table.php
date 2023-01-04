882d301ba4e7906639bab39329cfdada3e6b3668
Prateek Kher <prateek.kher@gmail.com>
2023-01-04 18:16:02 +0530

    Added Current Balance Column

--- database/migrations/2021_04_14_183222_create_ledgers_table.php
+++ database/migrations/2021_04_14_183222_create_ledgers_table.php
@@ -17,6 +17,7 @@ class CreateLedgersTable extends Migration
             $table->id();
             $table->string('title');
             $table->enum('kind', ['BANK', 'CASH', 'PAYABLES', 'RECEIVABLES', 'EXPENSE', 'INCOME']);
+            $table->double('balance');
             $table->timestamps();
         });
     }
