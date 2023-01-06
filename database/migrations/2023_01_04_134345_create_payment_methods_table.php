e8c250cffd84d7a0319380ed388601879399c2f3
Prateek Kher <prateek.kher@gmail.com>
2023-01-06 19:27:40 +0530

    renamed to payment_methods

--- database/migrations/2023_01_04_134345_create_payment_methods_table.php
+++ database/migrations/2023_01_04_134345_create_payment_methods_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaymentMethodTable extends Migration
+class CreatePaymentMethodsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePaymentMethodTable extends Migration
      */
     public function up()
     {
-        Schema::create('payment_method', function (Blueprint $table) {
+        Schema::create('payment_methods', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('invoice_id');
             $table->unsignedBigInteger('customer_id');
