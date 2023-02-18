3434496690e3940e124d7e3e51e985d411c14012
Prateek Kher <prateek.kher@gmail.com>
2023-02-18 17:25:42 +0530

    Changed displayName to title

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      * @var array
      */
     protected $fillable = [
-        'displayName', 'username', 'token', 'imageUrl', 'password'
+        'title', 'username', 'token', 'imageUrl', 'password', 'auth_level'
     ];
 
     /**
@@ -29,5 +29,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
      */
     protected $hidden = [
         'password'
+    ];
+
+    protected $casts = [
+        'auth_level' => 'integer'
     ];
 }
--- database/migrations/2021_04_12_130333_create_users_table.php
+++ database/migrations/2021_04_12_130333_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('displayName', 60);
+            $table->string('title', 60);
             $table->string('username', 50);
             $table->string('password');
             $table->string('token')->nullable()->default(NULL);
