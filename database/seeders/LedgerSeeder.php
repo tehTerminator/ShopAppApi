7572381d74993dc81f892f8497a81d6881af50f5
Prateek Kher <prateek.kher@gmail.com>
2023-03-29 08:57:59 +0530

    Updated Certain Wallets that were named as Bank

--- database/seeders/LedgerSeeder.php
+++ database/seeders/LedgerSeeder.php
@@ -14,17 +14,17 @@ class LedgerSeeder extends Seeder
      */
     public function run()
     {
-        Ledger::create(['title' => 'Walk-in Customer','kind' => 'RECEIVABLES']);
+        Ledger::create(['title' => 'Walk-in Customer','kind' => 'RECEIVABLE']);
         Ledger::create(['title' => 'SBI Current','kind' => 'BANK']);
         Ledger::create(['title' => 'HDFC Bank','kind' => 'BANK']);
-        Ledger::create(['title' => 'MPOnline','kind' => 'BANK']);
-        Ledger::create(['title' => 'CSC','kind' => 'BANK']);
+        Ledger::create(['title' => 'MPOnline','kind' => 'WALLET']);
+        Ledger::create(['title' => 'CSC','kind' => 'WALLET']);
         Ledger::create(['title' => 'Shop','kind' => 'CASH']);
         Ledger::create(['title' => 'Home','kind' => 'CASH']);
         Ledger::create(['title' => 'Electricity','kind' => 'EXPENSE']);
         Ledger::create(['title' => 'Internet','kind' => 'EXPENSE']);
         Ledger::create(['title' => 'Sales','kind' => 'INCOME']);
         Ledger::create(['title' => 'Cash','kind' => 'CASH']);
-        Ledger::create(['title' => 'Digipay','kind' => 'BANK']);
+        Ledger::create(['title' => 'Digipay','kind' => 'WALLET']);
     }
 }
