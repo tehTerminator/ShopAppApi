7d996e37a35d9da85506ae29f5ec63f512568bd0
Prateek Kher <prateek.kher@gmail.com>
2023-04-01 19:03:42 +0530

    Moved GeneralItem code from FrontEnd to Backend

---
+++ app/Services/GeneralItemService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use App\Models\Ledger;
+use App\Models\PosItem;
+
+class GeneralItemService
+{
+
+    private static $TYPE = [
+        'PRODUCT' => 0,
+        'LEDGER' => 1,
+        'BUNDLE' => 2
+    ];
+
+    public static function getItems()
+    {
+        $products = Product::select('id', 'title', 'rate')->get();
+        $ledgers = Ledger::select('id', 'title')->whereIn('kind', ['BANK', 'CASH', 'WALLET'])->get();
+        $bundles = PosItem::select('id', 'title', 'rate')->get();
+
+        $items = [];
+
+        foreach ($products as $item) {
+            $item['type'] = self::$TYPE['PRODUCT'];
+            array_push($items, $item);
+        }
+
+        foreach ($ledgers as $item) {
+            $item['type'] = self::$TYPE['LEDGER'];
+            $item['rate'] = 0;
+            array_push($items, $item);
+        }
+
+        foreach ($bundles as $item) {
+            $item['type'] = self::$TYPE['BUNDLE'];
+            array_push($items, $item);
+        }
+
+        return $items;
+    }
+}
