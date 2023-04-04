86589981608741fed5636f2255d7587a43fa4ca1
Prateek Kher <prateek.kher@gmail.com>
2023-04-04 23:11:17 +0530

    Modified Now Using Query without MySQL Functions

--- app/Http/Controllers/UserReportController.php
+++ app/Http/Controllers/UserReportController.php
@@ -22,78 +22,79 @@ class UserReportController extends Controller
         $this->date = Carbon::now();
     }
 
-    public function userWiseInvoiceCount(Request $request) {
-        
+    public function userWiseInvoiceCount(Request $request)
+    {
+
         if ($request->has('date')) {
             $this->date = $request->input('date');
         }
 
-        $response = Cache::remember('userWiseInvoiceCount' . $this->date, 300, function() {
+        $response = Cache::remember('userWiseInvoiceCount' . $this->date, 300, function () {
             return Invoice::select(
                 DB::raw('count(invoices.id) as value'),
                 DB::raw('users.displayName as name'),
             )
-            ->whereDate('invoices.created_at', $this->date)
-            ->join('users', 'users.id', '=', 'invoices.user_id')
-            ->groupBy('name')
-            ->get();
+                ->whereDate('invoices.created_at', $this->date)
+                ->join('users', 'users.id', '=', 'invoices.user_id')
+                ->groupBy('name')
+                ->get();
         });
 
         return response()->json($response);
     }
 
-    public function userWisePaymentCount(Request $request) {
-        
+    public function userWisePaymentCount(Request $request)
+    {
+
         if ($request->has('date')) {
             $this->date = $request->input('date');
         }
 
-        $response = Cache::remember('userWisePaymentCount' . $this->date, 300, function(){
+        $response = Cache::remember('userWisePaymentCount' . $this->date, 300, function () {
             return Transaction::select(
                 DB::raw(
-                "sum(
-                    calcTransactionAmount(
-                        `transactions`.`quantity`, `transactions`.`rate`, `transactions`.`discount`
-                    )
-                ) as 'value'"),
+                    "sum(
+                    `transactions`.`quantity` * `transactions`.`rate` * (1 - `transactions`.`discount`)
+                ) as 'value'"
+                ),
                 DB::raw("users.displayName as 'name'"),
             )
-            ->whereDate('transactions.created_at', $this->date)
-            ->where('transactions.item_type', 'LEDGER')
-            ->join('users', 'users.id', '=', 'transactions.user_id')
-            ->groupBy('name')
-            ->get();
+                ->whereDate('transactions.created_at', $this->date)
+                ->where('transactions.item_type', 'LEDGER')
+                ->join('users', 'users.id', '=', 'transactions.user_id')
+                ->groupBy('name')
+                ->get();
         });
         // ->toSql();
-                
+
         // return response($response);
         return response()->json($response);
     }
 
-    public function userWiseSalesCount(Request $request) {
-        
+    public function userWiseSalesCount(Request $request)
+    {
+
         if ($request->has('date')) {
             $this->date = $request->input('date');
         }
 
-        $response = Cache::remember('userWiseSalesCount' . $this->date, 300, function() {
+        $response = Cache::remember('userWiseSalesCount' . $this->date, 300, function () {
             return Transaction::select(
                 DB::raw(
-                "sum(
-                    calcTransactionAmount(
-                        `transactions`.`quantity`, `transactions`.`rate`, `transactions`.`discount`
-                    )
-                ) as 'value'"),
+                    "sum(
+                    `transactions`.`quantity` * `transactions`.`rate` * (1 - `transactions`.`discount`)
+                ) as 'value'"
+                ),
                 DB::raw("users.displayName as 'name'"),
             )
-            ->whereDate('transactions.created_at', $this->date)
-            ->where('transactions.item_type', 'PRODUCT')
-            ->join('users', 'users.id', '=', 'transactions.user_id')
-            ->groupBy('name')
-            ->get();
+                ->whereDate('transactions.created_at', $this->date)
+                ->where('transactions.item_type', 'PRODUCT')
+                ->join('users', 'users.id', '=', 'transactions.user_id')
+                ->groupBy('name')
+                ->get();
         });
         // ->toSql();
-                
+
         // return response($response);
         return response()->json($response);
     }
