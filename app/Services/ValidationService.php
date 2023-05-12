1b452ec94a4ad144fba23766b51cd0ca0a9a1516
Prateek Kher <prateek.kher@gmail.com>
2023-05-12 14:55:30 +0530

    Created Validation Service

---
+++ app/Services/ValidationService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ValidationService
+{
+    // Define specific validation rules based on the table and operation type
+
+    function validateModel(Request $request, $table, $operationType = 'create')
+    {
+        // Get the validation rules for the specified table and operation type
+        $validationRules = include('./../Models/Validation/' . $table . 'ValidationRules.php');
+
+        // Get the specific validation rules based on the operation type
+        $rules = $validationRules[$operationType];
+
+        // Run validation
+        $validator = Validator::make($request->all(), $rules);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            // Return the validation error messages
+            return $validator->errors();
+        }
+
+        // Validation passed
+        return true;
+    }
+
+    public function __construct()
+    {
+    }
+}
